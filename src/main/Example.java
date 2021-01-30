77c8ad11a8489a60b049570a3d23a95485422ca0
pletosu.lilian <lilianzau@mail.ru>
2021-01-30 22:21:59 +0200

    Первый пример простой программы

---
+++ src/main/Example.java
@@ -0,0 +1,11 @@
+package main;
+/*
+Это простая программа на Java.
+Присвоить исходному файлу имя "Example.java"
+ */
+public class Example {
+    // Эта программа начинается с с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java.");
+    }
+}
