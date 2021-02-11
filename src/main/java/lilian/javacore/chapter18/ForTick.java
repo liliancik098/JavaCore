e666d4dcb4564d13b0a46cd78e5109079eaa8e2e
pletosu.lilian <lilianzau@mail.ru>
2021-02-11 11:17:42 +0200

    Разбор кода

---
+++ src/main/java/lilian/javacore/chapter18/ForTick.java
@@ -0,0 +1,12 @@
+package main.java.lilian.javacore.chapter18;
+// Демонстрация применения опретаора цикла for
+public class ForTick {
+    public static void main(String[] args) {
+
+
+        for (int n = 10; n > 0; n--)
+            System.out.println("такт " + n);
+        for (int n=10; n>0; n--)
+            System.out.println("такт " + n);
+    }
+}
---
+++ src/main/java/lilian/javacore/chapter18/ForVar.java
@@ -0,0 +1,15 @@
+package main.java.lilian.javacore.chapter18;
+// Отдельные части оператора цикла for могут отсутствовать
+public class ForVar {
+    public static void main(String[] args) {
+        int i;
+        boolean done = false;
+        i = 0;
+
+        for ( ; !done; ) {
+            System.out.println("i равно " + i);
+            if (i == 10) done = true;
+            i++;
+        }
+    }
+}
