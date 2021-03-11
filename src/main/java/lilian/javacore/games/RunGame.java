401a888e1d1130336b183611297a505f2c64d86e
lilianpletosu <1>
2021-03-11 10:58:59 +0200

    Игра(изменения)

--- src/main/java/lilian/javacore/games/RunGame.java
+++ src/main/java/lilian/javacore/games/RunGame.java
@@ -2,7 +2,7 @@ package main.java.lilian.javacore.games;
 
 public class RunGame {
     public static void main(String[] args) {
-        GameLogic start = new GameLogic();
+        GameLogic start = new GameLogic("Lilian");
         start.startGame();
     }
 }
